13c46b306911a1ccc232a76bf035b2d67f028235
KodeStar <admin@Xenon.local>
2018-01-29 19:50:59 +0000

    change database connection details

--- config/database.php
+++ config/database.php
@@ -35,7 +35,8 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            //'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' => database_path(env('DB_DATABASE'))
             'prefix' => '',
         ],
 
