7c859b629eb761cdff08b3ccb71910b2e892af76
Chris <chris@headred.net>
2018-02-02 15:29:17 +0000

    item sass

--- resources/views/item.blade.php
+++ resources/views/item.blade.php
@@ -1,7 +1,7 @@
                     <section class="item-container">
                         <div class="item" style="background-color: {{ $app->colour }}">
                             @if($app->icon)
-                            <img src="{{ asset('storage/'.$app->icon) }}" />
+                            <img class="app-icon" src="{{ asset('storage/'.$app->icon) }}" />
                             @else
                             <i class="fas fa-app-store-ios"></i>
                             @endif
