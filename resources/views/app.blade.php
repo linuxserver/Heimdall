422d0bee9a25e18dc77972d6cdc44c94bec01c41
KodeStar <admin@Xenon.local>
2018-02-03 19:21:01 +0000

    removed protocol from CDN javascript

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -68,9 +68,9 @@
 
             </div>
         </div>
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script>!window.jQuery && document.write('<script src="/js/jquery-3.3.1.min.js"><\/script>')</script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
         <script src="/js/app.js"></script>
         
     </body>
