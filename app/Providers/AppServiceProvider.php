eb3c3b94d667121b197b9992c6f35661f8ace332
Chris <chris@headred.net>
2018-02-05 15:22:51 +0000

    hopefully fix upgrade issues from v1.0

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Artisan;
+use Schema;
 use App\Setting;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,16 +28,18 @@ class AppServiceProvider extends ServiceProvider
             //Artisan::call('route:cache');
         }
         if(is_file(database_path(env('DB_DATABASE')))) {
-            if($bg_image = Setting::fetch('background_image')) {
-                $alt_bg = ' style="background-image: url('.asset('storage/'.$bg_image).')"';
-            }
+            if(Schema::hasTable('settings')) {
+                if($bg_image = Setting::fetch('background_image')) {
+                    $alt_bg = ' style="background-image: url('.asset('storage/'.$bg_image).')"';
+                }
 
-            // check version to see if an upgrade is needed
-            $db_version = Setting::fetch('version');
-            $app_version = config('app.version');
-            if(version_compare($app_version, $db_version) == 1) { // app is higher than db, so need to run migrations etc
-                Artisan::call('migrate', array('--path' => 'database/migrations', '--force' => true, '--seed' => true));                   
-            }
+                // check version to see if an upgrade is needed
+                $db_version = Setting::fetch('version');
+                $app_version = config('app.version');
+                if(version_compare($app_version, $db_version) == 1) { // app is higher than db, so need to run migrations etc
+                    Artisan::call('migrate', array('--path' => 'database/migrations', '--force' => true, '--seed' => true));                   
+                }
+            } 
         }
         view()->share('alt_bg', $alt_bg);
 
