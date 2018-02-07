7376aef136841b2735c85fe07afda175f1dfc6f3
aptalca <aptalca@users.noreply.github.com>
2018-02-07 18:08:12 -0500

    formatting

--- resources/lang/tr/passwords.php
+++ resources/lang/tr/passwords.php
@@ -1,5 +1,7 @@
 <?php
+
 return [
+    
     /*
     |--------------------------------------------------------------------------
     | Password Reset Language Lines
@@ -10,9 +12,11 @@ return [
     | has failed, such as for an invalid token or invalid new password.
     |
     */
+    
     'password' => 'Şifre en az altı karakter olmalı ve onaylamasına uymalıdır.',
     'reset' => 'Şifreniz sıfırlandı!',
-    'sent' => 'Şifre sıfırlama bağlantısı adresinize yollandı!',
+    'sent' => 'Şifre sıfırlama bağlantısı eposta adresinize yollandı!',
     'token' => 'Şifre sıfırlama simgesi geçerli değil.',
     'user' => "Adresle ilişkili kullanıcı adı bulunamadı.",
+    
 ];
