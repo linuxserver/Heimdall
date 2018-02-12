c302c26d8703e38acf565aa354297ae3d3f602b2
Jonas Myhr Refseth <jonas@oisann.net>
2018-02-12 19:26:31 +0100

    Translated most of the English language files into Norwegian. The messages in validation.php could be hard to translate.

---
+++ resources/lang/no/app.php
@@ -0,0 +1,80 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | App Language Lines
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'settings.system' => 'Systemet',
+    'settings.appearance' => 'Utseende',
+    'settings.miscellaneous' => 'Diverse',
+
+    'settings.version' => 'Versjon',
+    'settings.background_image' => 'Bakgrunnsbilde',
+    'settings.homepage_search' => 'Startside Søk',
+    'settings.search_provider' => 'Søkemotor',
+    'settings.language' => 'Språk',
+    'settings.reset' => 'Tilbakestill tilbake til standard',
+    'settings.remove' => 'Fjern',
+    'settings.search' => 'søk',
+    'settings.no_items' => 'Ingen funn',
+    
+
+    'settings.label' => 'Merkelapp',    
+    'settings.value' => 'Verdi',    
+    'settings.edit' => 'Endre',    
+    'settings.view' => 'Se',    
+
+    'options.none' => '- ikke valgt -',
+    'options.google' => 'Google',
+    'options.ddg' => 'DuckDuckGo',
+    'options.bing' => 'Bing',
+    'options.yes' => 'Ja',
+    'options.no' => 'Nei',
+
+    'buttons.save' => 'Lagre',
+    'buttons.cancel' => 'Avbryt',
+    'buttons.add' => 'Legg til',
+    'buttons.upload' => 'Last opp en fil',
+
+    'dash.pin_item' => 'Fest objektet til dashboardet',
+    'dash.no_apps' => 'Det er for øyeblikket ingen festede applikasjoner, :link1 eller :link2',
+    'dash.link1' => 'Legg til en applikasjon her',
+    'dash.link2' => 'Fest et objekt til dashboardet',
+    'dash.pinned_items' => 'Festede Objekter',
+
+    'apps.app_list' => 'Applikasjonsliste',
+    'apps.view_trash' => 'Vis papirkurven',
+    'apps.add_application' => 'Legg til en applikasjon',
+    'apps.application_name' => 'Applikasjonsnavn',
+    'apps.colour' => 'Farge',
+    'apps.icon' => 'Ikon',
+    'apps.pinned' => 'Festet',
+    'apps.title' => 'Tittel',
+    'apps.hex' => 'Hex-farge',
+    'apps.username' => 'Brukernavn',
+    'apps.password' => 'Passord',
+    'apps.config' => 'Konfigurasjon',
+    'apps.apikey' => 'Api nøkkel',
+    'apps.enable' => 'Aktiver',
+
+    'url' => 'Url',
+    'title' => 'Tittel',
+    'delete' => 'Slett',    
+    'optional' => 'Valgfritt',    
+    'restore' => 'Tilbakestill',    
+
+    'alert.success.item_created' => 'Objektet ble opprettet',
+    'alert.success.item_updated' => 'Objektet ble oppdatert',
+    'alert.success.item_deleted' => 'Objektet ble slettet',
+    'alert.success.item_restored' => 'Objektet ble gjenopprettet',
+
+    'alert.success.setting_updated' => 'Du har oppdatert denne innstillingen',
+    'alert.error.not_exist' => 'Denne innstillingen eksisterer ikke.',
+
+
+];
