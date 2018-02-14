0f9b1cdf2565e32b274daafd123dc5ade6e3e563
Futos <futosone@googlemail.com>
2018-02-14 01:07:44 +0100

    fix(SupportedApps): fixed Item.php
    Fixed Item.php because forgot to save the changes before push
    
    Closes #65

--- app/SupportedApps/Netdata.php
+++ app/SupportedApps/Netdata.php
@@ -1,12 +1,12 @@
 <?php namespace App\SupportedApps;
 
-class HomeAssistant implements Contracts\Applications {
+class Netdata implements Contracts\Applications {
     public function defaultColour()
     {
-        return '#073c52';
+        return '#543737';
     }
     public function icon()
     {
-        return 'supportedapps/homeassistant.png';
+        return 'supportedapps/netdata.png';
     }
 }
