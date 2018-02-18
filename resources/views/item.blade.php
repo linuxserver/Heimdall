249f437ff5dcb4d52cbb973325632e5a685adf95
KodeStar <admin@Xenon.local>
2018-02-18 18:38:43 +0000

    fixes to RP

--- resources/views/item.blade.php
+++ resources/views/item.blade.php
@@ -13,6 +13,6 @@
                             </div>
                             <a class="link"{{ $app->target }} href="{{ $app->link }}"><i class="fas {{ $app->link_icon }}"></i></a>
                         </div>
-                        <a class="item-edit" href="{{ route($app->link_type.'.edit', $app->id) }}"><i class="fas fa-pencil"></i></a>
+                        <a class="item-edit" href="{{ route($app->link_type.'.edit', [ $app->id ], false) }}"><i class="fas fa-pencil"></i></a>
                         
                     </section>
