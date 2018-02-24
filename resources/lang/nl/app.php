f1a6feeb8fe68ae20317f255f32da920732fe4b6
Ephelyon <gherkin666@hotmail.com>
2018-02-24 18:54:31 +0000

    Add Dutch translation

---
+++ resources/lang/nl/app.php
@@ -0,0 +1,89 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | App Language Lines
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'settings.system' => 'Systeem',
+    'settings.appearance' => 'Uiterlijk',
+    'settings.miscellaneous' => 'Overige',
+
+    'settings.version' => 'Versie',
+    'settings.background_image' => 'Achtergrondafbeelding',
+    'settings.homepage_search' => 'Zoeken op thuispagina',
+    'settings.search_provider' => 'Zoekaanbieder',
+    'settings.language' => 'Taal',
+    'settings.reset' => 'Op standaard instellen',
+    'settings.remove' => 'Verwijderen',
+    'settings.search' => 'zoeken',
+    'settings.no_items' => 'Geen items gevonden',
+    
+
+    'settings.label' => 'Label',    
+    'settings.value' => 'Waarde',    
+    'settings.edit' => 'Bewerken',    
+    'settings.view' => 'Weergeven',    
+
+    'options.none' => '- niet ingesteld -',
+    'options.google' => 'Google',
+    'options.ddg' => 'DuckDuckGo',
+    'options.bing' => 'Bing',
+    'options.yes' => 'Ja',
+    'options.no' => 'Nee',
+
+    'buttons.save' => 'Opslaan',
+    'buttons.cancel' => 'Annuleren',
+    'buttons.add' => 'Toevoegen',
+    'buttons.upload' => 'Bestand uploaden',
+
+    'dash.pin_item' => 'Item aan dashboard vastmaken',
+    'dash.no_apps' => 'Er zijn momenteel geen vastgemaakte toepassingen, :link1 of :link2',
+    'dash.link1' => 'Voeg hier een toepassing toe',
+    'dash.link2' => 'Een item aan het dashboard vastmaken',
+    'dash.pinned_items' => 'Vastgemaakte Items',
+
+    'apps.app_list' => 'Lijst met toepassingen',
+    'apps.view_trash' => 'Prullenbak weergeven',
+    'apps.add_application' => 'Toepassing toevoegen',
+    'apps.application_name' => 'Naam van toepassing',
+    'apps.colour' => 'Kleur',
+    'apps.icon' => 'Pictogram',
+    'apps.pinned' => 'Vastgemaakt',
+    'apps.title' => 'Titel',
+    'apps.hex' => 'Hex-kleur',
+    'apps.username' => 'Gebruikersnaam',
+    'apps.password' => 'Wachtwoord',
+    'apps.config' => 'Configuratie',
+    'apps.apikey' => 'API-sleutel',
+    'apps.enable' => 'Inschakalen',
+    'apps.tag_list' => 'Lijst met tags',
+    'apps.add_tag' => 'Tag toevoegen',
+    'apps.tag_name' => 'Naam van tag',
+    'apps.tags' => 'Tags',
+
+    'url' => 'URL',
+    'title' => 'Titel',
+    'delete' => 'Verwijderen',    
+    'optional' => 'Optioneel',    
+    'restore' => 'Herstellen',    
+
+    'alert.success.item_created' => 'Item met succes aangemaakt',
+    'alert.success.item_updated' => 'Item met succes bewerkt',
+    'alert.success.item_deleted' => 'Item met succes verwijderd',
+    'alert.success.item_restored' => 'Item met succes hersteld',
+
+    'alert.success.tag_created' => 'Tag met succes aangemaakt',
+    'alert.success.tag_updated' => 'Tag met succes bewerkt',
+    'alert.success.tag_deleted' => 'Tag met succes verwijderd',
+    'alert.success.tag_restored' => 'Tag met succes hersteld',
+
+    'alert.success.setting_updated' => 'Deze instelling is met succes gewijzigd',
+    'alert.error.not_exist' => 'Deze instelling bestaat niet.',
+
+
+];
---
+++ resources/lang/nl/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Wachtwoorden moeten tenminste zes karakters bevatten en overeenkomen met de bevestiging.',
+    'reset' => 'Uw wachtwoord is opnieuw ingesteld!',
+    'sent' => 'De link voor het opnieuw instellen van uw wachtwoord is naar u gemaild!',
+    'token' => 'Deze token voor het opnieuw instellen van een wachtwoord is ongeldig.',
+    'user' => "Er bestaat geen gebruiker met het opgegeven e-mailadres.",
+
+];
