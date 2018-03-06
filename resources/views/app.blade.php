c4a4d25f7e4a8d387b60ffa9a5939126f33e3db3
Chris <chris@headred.net>
2018-03-06 09:43:54 +0000

    change js version to break cache

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -88,7 +88,7 @@
         <script src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script>!window.jQuery && document.write('<script src="/js/jquery-3.3.1.min.js"><\/script>')</script>
         <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-        <script src="/js/app.js"></script>
+        <script src="/js/app.js?v=2"></script>
         @yield('scripts')
         
     </body>
