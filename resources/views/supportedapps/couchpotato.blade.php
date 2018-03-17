ae391b885bc6d7333d306ddcbc891666b6f70117
KodeStar <kodestar@gmail.com>
2018-03-17 13:17:45 +0000

    added missing dataonly attribute so it polls every 20 seconds

--- resources/views/supportedapps/couchpotato.blade.php
+++ resources/views/supportedapps/couchpotato.blade.php
@@ -1,6 +1,7 @@
 <h2>{{ __('app.apps.config') }} ({{ __('app.optional') }})</h2>
 <div class="items">
     <input type="hidden" name="config[enabled]" value="1" />
+    <input type="hidden" data-config="dataonly" class="config-item" name="config[dataonly]" value="1" />
     <input type="hidden" data-config="type" class="config-item" name="config[type]" value="\App\SupportedApps\CouchPotato" />
     <div class="input">
         <label>{{ __('app.apps.apikey') }}</label>
