dc86d636acfdc3ba33fc573329741cc9cde14e92
halorrr <mvmenanno@hotmail.ca>
2018-03-18 09:20:39 -0400

    Create Watcher3.php

---
+++ app/SupportedApps/Watcher3.php
@@ -0,0 +1,12 @@
+
+<?php namespace App\SupportedApps;
+class Watcher3 implements Contracts\Applications {
+    public function defaultColour()
+    {
+        return '#500';
+    }
+    public function icon()
+    {
+        return 'supportedapps/watcher3.png';
+    }
+}
