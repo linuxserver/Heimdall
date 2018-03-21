17eef7a4aa87ff80e0b97a17c8e1dd78013f33a5
halorrr <mvmenanno@hotmail.ca>
2018-03-21 11:17:12 -0400

    Update pyLoad.php

--- app/SupportedApps/pyLoad.php
+++ app/SupportedApps/pyLoad.php
@@ -1,12 +1,12 @@
 
 <?php namespace App\SupportedApps;
-class Krusader implements Contracts\Applications {
+class pyLoad implements Contracts\Applications {
     public function defaultColour()
     {
-        return '#5A5';
+        return '#881';
     }
     public function icon()
     {
-        return 'supportedapps/krusader.png';
+        return 'supportedapps/pyload.png';
     }
 }
