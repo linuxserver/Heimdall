4738d7c951b4f4b398acc277a91629b3115325ec
halorrr <mvmenanno@hotmail.ca>
2018-03-21 11:07:16 -0400

    Create pyLoad.php

---
+++ app/SupportedApps/pyLoad.php
@@ -0,0 +1,12 @@
+
+<?php namespace App\SupportedApps;
+class Krusader implements Contracts\Applications {
+    public function defaultColour()
+    {
+        return '#5A5';
+    }
+    public function icon()
+    {
+        return 'supportedapps/krusader.png';
+    }
+}
