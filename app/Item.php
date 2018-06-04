c446b8a5afc2b7be696b48c6934d88c02fdae34a
Magnus Ullberg <magnus@ullberg.us>
2018-06-04 19:12:24 -0400

    Added MusicBrainz support.

--- app/Item.php
+++ app/Item.php
@@ -44,6 +44,7 @@ class Item extends Model
             'Lidarr' => \App\SupportedApps\Lidarr::class,
             'Mcmyadmin' => \App\SupportedApps\Mcmyadmin::class,
             'Medusa' => \App\SupportedApps\Medusa::class,
+            'MusicBrainz' => \App\SupportedApps\MusicBrainz::class,
             'NZBGet' => \App\SupportedApps\Nzbget::class,
             'Netdata' => \App\SupportedApps\Netdata::class,
             'Nextcloud' => \App\SupportedApps\Nextcloud::class,
