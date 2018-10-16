bcc1ddaca893adfa0c8bac50528ab58cdefa6c0c
alxlaxv <35723598+alxlaxv@users.noreply.github.com>
2018-10-16 20:03:22 +0200

    Create sonarr.blade.php

---
+++ resources/views/supportedapps/sonarr.blade.php
@@ -0,0 +1,11 @@
+<h2>{{ __('app.apps.config') }} ({{ __('app.optional') }})</h2>
+<div class="items">
+    <input type="hidden" data-config="type" class="config-item" name="config[type]" value="\App\SupportedApps\Sonarr" />
+    <div class="input">
+        <label>{{ __('app.apps.apiKey') }}</label>
+        {!! Form::text('config[apiKey]', null, array('placeholder' => __('app.apps.apiKey'), 'data-config' => 'apiKey', 'class' => 'form-control config-item')) !!}
+    </div>
+    <div class="input">
+        <button style="margin-top: 32px;" class="btn test" id="test_config">Test</button>
+    </div>
+</div>
