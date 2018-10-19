0727b3a7b3d2323dfece65b0f4e9279e40993b1a
alxlaxv <35723598+alxlaxv@users.noreply.github.com>
2018-10-19 22:17:59 +0200

    Update sonarr.blade.php

--- resources/views/supportedapps/sonarr.blade.php
+++ resources/views/supportedapps/sonarr.blade.php
@@ -1,6 +1,7 @@
 <h2>{{ __('app.apps.config') }} ({{ __('app.optional') }})</h2>
 <div class="items">
     <input type="hidden" data-config="type" class="config-item" name="config[type]" value="\App\SupportedApps\Sonarr" />
+    <input type="hidden" data-config="dataonly" class="config-item" name="config[dataonly]" value="1" />
     <div class="input">
         <label>{{ strtoupper(__('app.url')) }}</label>
         {!! Form::text('config[override_url]', null, array('placeholder' => __('app.apps.override'), 'id' => 'override_url', 'class' => 'form-control')) !!}
@@ -8,6 +9,19 @@
     <div class="input">
         <label>{{ __('app.apps.apiKey') }}</label>
         {!! Form::text('config[apiKey]', null, array('placeholder' => __('app.apps.apiKey'), 'data-config' => 'apiKey', 'class' => 'form-control config-item')) !!}
+    </div>
+    <div class="input">
+        <label>{{ __('app.apps.enable') }}</label>
+        {!! Form::hidden('config[enabled]', '0') !!}
+        <label class="switch">
+            <?php
+            $checked = false;
+            if(isset($item->config->enabled) && (bool)$item->config->enabled === true) $checked = true;
+            $set_checked = ($checked) ? ' checked="checked"' : '';
+            ?>
+            <input type="checkbox" name="config[enabled]" value="1"<?php echo $set_checked;?> />
+            <span class="slider round"></span>
+        </label>
     </div>
     <div class="input">
         <button style="margin-top: 32px;" class="btn test" id="test_config">Test</button>
