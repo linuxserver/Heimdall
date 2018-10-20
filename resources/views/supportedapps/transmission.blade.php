7b9d3f0ec6445a1195ee0c7821e7e393350c5583
Kode <kodestar@gmail.com>
2018-10-20 20:08:36 +0100

    replace text input with password input and swap input for Form::password

--- resources/views/supportedapps/transmission.blade.php
+++ resources/views/supportedapps/transmission.blade.php
@@ -12,7 +12,7 @@
     </div>
     <div class="input">
         <label>{{ __('app.apps.password') }}</label>
-        {!! Form::text('config[password]', null, array('placeholder' => __('app.apps.password'), 'data-config' => 'password', 'class' => 'form-control config-item')) !!}
+        {!! Form::password('config[password]', null, array('placeholder' => __('app.apps.password'), 'data-config' => 'password', 'class' => 'form-control config-item')) !!}
     </div>
     <div class="input">
         <button style="margin-top: 32px;" class="btn test" id="test_config">Test</button>
