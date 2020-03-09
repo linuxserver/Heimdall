365de9512f1d733fa50874df5a3f670cc9047e90
sKai <skai-zhang@hotmail.com>
2020-03-09 20:57:31 +0800

    Creat new language
    Creat simplified chinese , apply to china

---
+++ resources/lang/cn/app.php
@@ -0,0 +1,120 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | App Language Lines
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'settings.system' => '系统',
+    'settings.appearance' => '外观',
+    'settings.miscellaneous' => '其他',
+    'settings.support' => '支持',
+    'settings.donate' => '捐赠',
+
+    'settings.version' => '版本',
+    'settings.background_image' => '背景图片',
+    'settings.window_target' => '链接打开方式',
+    'settings.window_target.current' => '在当前选项卡中打开',
+    'settings.window_target.one' => '在同一个选项卡中打开',
+    'settings.window_target.new' => '在新的选项卡中打开',
+    'settings.homepage_search' => '主页搜索',
+    'settings.search_provider' => '默认搜索引擎',
+    'settings.language' => '语言',
+    'settings.reset' => '重置为默认值',
+    'settings.remove' => '删除',
+    'settings.search' => '搜索',
+    'settings.no_items' => '没有可用项目',
+
+
+    'settings.label' => '设置项',
+    'settings.value' => '内容',
+    'settings.edit' => '编辑',
+    'settings.view' => '查看',
+
+    'options.none' => '- 未设置 -',
+    'options.google' => 'Google',
+    'options.ddg' => 'DuckDuckGo',
+    'options.bing' => 'Bing',
+    'options.qwant' => 'Qwant',
+    'options.startpage' => 'StartPage',
+    'options.yes' => '是',
+    'options.no' => '否',
+    'options.nzbhydra' => 'NZBHydra',
+    'options.jackett' => 'Jackett',
+
+    'buttons.save' => '保存',
+    'buttons.cancel' => '取消',
+    'buttons.add' => '添加',
+    'buttons.upload' => '上传图标',
+    'buttons.downloadapps' => '更新应用列表',
+
+    'dash.pin_item' => '将应用固定到仪表盘',
+    'dash.no_apps' => '当前没有固定的应用, 请 :link1 或 :link2',
+    'dash.link1' => '添加应用',
+    'dash.link2' => '将应用固定到仪表盘',
+    'dash.pinned_items' => '固定应用',
+
+    'apps.app_list' => '应用列表',
+    'apps.view_trash' => '查看垃圾箱',
+    'apps.add_application' => '添加应用',
+    'apps.application_name' => '应用名称',
+    'apps.colour' => '颜色',
+    'apps.icon' => '图标',
+    'apps.pinned' => '固定',
+    'apps.title' => '标题',
+    'apps.hex' => 'Hex颜色',
+    'apps.username' => '用户名称',
+    'apps.password' => '密码',
+    'apps.config' => '配置',
+    'apps.apikey' => 'API 密钥',
+    'apps.enable' => 'Enable',
+    'apps.tag_list' => '标签列表',
+    'apps.add_tag' => '添加标签',
+    'apps.tag_name' => '标签名称',
+    'apps.tags' => '标签',
+    'apps.override' => '如果与主链接不同',
+    'apps.preview' => '预览',
+    'apps.apptype' => '应用类型',
+
+    'dashboard' => '主页-控制台',
+
+    'user.user_list' => '用户',
+    'user.add_user' => '添加用户',
+    'user.username' => '用户名称',
+    'user.avatar' => '头像',
+    'user.email' => '邮箱地址',
+    'user.password_confirm' => '重复密码',
+    'user.secure_front' => '仅设置密码后才允许公开访问.',
+    'user.autologin' => '仅可从特定的链接访问(任何通过链接都可访问).',
+
+    'url' => '链接',
+    'title' => '标题',
+    'delete' => '删除',
+    'optional' => '可选',
+      'restore' => '复位',
+
+    'alert.success.item_created' => '应用添加成功',
+    'alert.success.item_updated' => '应用更新成功',
+    'alert.success.item_deleted' => '应用删除成功',
+    'alert.success.item_restored' => '应用复位成功',
+    'alert.success.updating' => '更新应用列表完成',
+
+    'alert.success.tag_created' => '标签添加成功',
+    'alert.success.tag_updated' => '标签更新成功',
+    'alert.success.tag_deleted' => '标签删除成功',
+    'alert.success.tag_restored' => '标签复位成功',
+
+    'alert.success.setting_updated' => '你已成功编辑此设置',
+    'alert.error.not_exist' => '此设置项不存在.',
+
+    'alert.success.user_created' => '用户添加成功',
+    'alert.success.user_updated' => '用户更新成功',
+    'alert.success.user_deleted' => '用户删除成功',
+    'alert.success.user_restored' => '用户复位成功',
+
+
+];
