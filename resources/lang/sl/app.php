0292e9976ef7c47be3c7208ea65c41f084a95a7f
AndrazKorenc <andy.mann.tk@gmail.com>
2020-03-15 09:50:17 +0100

    added Slovenian translation

---
+++ resources/lang/sl/app.php
@@ -0,0 +1,120 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | App Language Lines
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'settings.system' => 'Sistem',
+    'settings.appearance' => 'Videz',
+    'settings.miscellaneous' => 'Ostalo',
+    'settings.support' => 'Podpora',
+    'settings.donate' => 'Doniraj',
+
+    'settings.version' => 'Verzija',
+    'settings.background_image' => 'Slika za ozadje',
+    'settings.window_target' => 'Povezava se odpre v',
+    'settings.window_target.current' => 'Odpri v tem zavihku',
+    'settings.window_target.one' => 'Odpri v istem zavihku',
+    'settings.window_target.new' => 'Odpri v novem zavihku',
+    'settings.homepage_search' => 'Iskanje po domači strani',
+    'settings.search_provider' => 'Privzeti ponudnik iskanja',
+    'settings.language' => 'Jezik',
+    'settings.reset' => 'Ponastavi na privzeto',
+    'settings.remove' => 'Odstrani',
+    'settings.search' => 'išči',
+    'settings.no_items' => 'Ni najdenih elementov',
+
+
+    'settings.label' => 'Oznaka',
+    'settings.value' => 'Vrednost',
+    'settings.edit' => 'Uredi',
+    'settings.view' => 'Poglej',
+
+    'options.none' => '- ni nastavljeno -',
+    'options.google' => 'Google',
+    'options.ddg' => 'DuckDuckGo',
+    'options.bing' => 'Bing',
+    'options.qwant' => 'Qwant',
+    'options.startpage' => 'StartPage',
+    'options.yes' => 'Da',
+    'options.no' => 'Ne',
+    'options.nzbhydra' => 'NZBHydra',
+    'options.jackett' => 'Jackett',
+
+    'buttons.save' => 'Shrani',
+    'buttons.cancel' => 'Prekliči',
+    'buttons.add' => 'Dodaj',
+    'buttons.upload' => 'Naloži ikono',
+    'buttons.downloadapps' => 'Posodobi Seznam Aplikacij',
+
+    'dash.pin_item' => 'Pripni element na nadzorno ploščo',
+    'dash.no_apps' => 'Trenutno ni pripetih aplikacij, :link1 ali :link2',
+    'dash.link1' => 'Dodaj aplikacijo tukaj',
+    'dash.link2' => 'Pripni aplikacijo na nadzorno ploščo',
+    'dash.pinned_items' => 'Pripeti elementi',
+
+    'apps.app_list' => 'Seznam aplikacij',
+    'apps.view_trash' => 'Poglej smeti',
+    'apps.add_application' => 'Dodaj aplikacijo',
+    'apps.application_name' => 'Ime aplikacije',
+    'apps.colour' => 'Barva',
+    'apps.icon' => 'Ikona',
+    'apps.pinned' => 'Pripeto',
+    'apps.title' => 'Naslov',
+    'apps.hex' => 'Hex barva',
+    'apps.username' => 'Uporabniško ime',
+    'apps.password' => 'Geslo',
+    'apps.config' => 'Konfiguracija',
+    'apps.apikey' => 'API Ključ',
+    'apps.enable' => 'Omogoči',
+    'apps.tag_list' => 'Seznam oznak',
+    'apps.add_tag' => 'Dodaj oznako',
+    'apps.tag_name' => 'Ime oznake',
+    'apps.tags' => 'Oznake',
+    'apps.override' => 'Če je drugače od glavnega url-ja',
+    'apps.preview' => 'Predogled',
+    'apps.apptype' => 'Tip aplikacije',
+
+    'dashboard' => 'Home dashboard',
+
+    'user.user_list' => 'Uporabniki',
+    'user.add_user' => 'Dodaj uporabnika',
+    'user.username' => 'Uporabniško ime',
+    'user.avatar' => 'Avatar',
+    'user.email' => 'Email',
+    'user.password_confirm' => 'Potrdi geslo',
+    'user.secure_front' => 'Dovoli javni dostop do strani - Samo če je nastavljeno geslo.',
+    'user.autologin' => 'Dovoli prijavo iz specifičnega URL-ja. Vsi s povezavo se lahko prijavijo.',
+
+    'url' => 'URL',
+    'title' => 'Naslov',
+    'delete' => 'Izbriši',
+    'optional' => 'Opcijsko',
+    'restore' => 'Obnovi',
+
+    'alert.success.item_created' => 'Element uspešno ustvarjen',
+    'alert.success.item_updated' => 'Element uspešno posodobljen',
+    'alert.success.item_deleted' => 'Element uspešno izbrisan',
+    'alert.success.item_restored' => 'Element uspešno obnovljen',
+    'alert.success.updating' => 'Posodabljam seznam aplikacij',
+
+    'alert.success.tag_created' => 'Oznaka uspešno ustvarjena',
+    'alert.success.tag_updated' => 'Oznaka uspešno posodobljena',
+    'alert.success.tag_deleted' => 'Oznaka uspešno izbrisana',
+    'alert.success.tag_restored' => 'Oznaka uspešno obnovljena',
+
+    'alert.success.setting_updated' => 'Uspešno uredil to oznako',
+    'alert.error.not_exist' => 'Ta nastavitev ne obstaja.',
+
+    'alert.success.user_created' => 'Uporabnik uspešno ustvarjen',
+    'alert.success.user_updated' => 'Uporabnik uspešno posodobljen',
+    'alert.success.user_deleted' => 'Uporabnik uspešno izbrisan',
+    'alert.success.user_restored' => 'Uporabnik uspešno obnovljen',
+
+
+];
