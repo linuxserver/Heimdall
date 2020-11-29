27a667675ab9553e999e68adbb77d0d892555256
bence192 <75220209+bence192@users.noreply.github.com>
2020-11-29 19:57:27 +0100

    Create app.php

---
+++ resources/lang/hu/app.php
@@ -0,0 +1,121 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | App Language Lines
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'settings.system' => 'Rendszer',
+    'settings.appearance' => 'Megjelenés',
+    'settings.miscellaneous' => 'Miscellaneous',
+    'settings.support' => 'Támogatás',
+    'settings.donate' => 'Adomány',
+
+    'settings.version' => 'Verzió',
+    'settings.background_image' => 'Háttérkép',
+    'settings.window_target' => 'Link megnyitása',
+    'settings.window_target.current' => 'Ezen a lapon',
+    'settings.window_target.one' => 'Azonos lapon',
+    'settings.window_target.new' => 'Új lapon',
+    'settings.homepage_search' => 'Kezdő oldal kereső',
+    'settings.search_provider' => 'Alapértelmezett kereső motor	',
+    'settings.language' => 'Nyelv',
+    'settings.reset' => 'Alapértelmezetek visszaállítása',
+    'settings.remove' => 'Eltávolítás',
+    'settings.search' => 'keresés',
+    'settings.no_items' => 'Nincs találat',
+
+
+    'settings.label' => 'Címke',
+    'settings.value' => 'Érték',
+    'settings.edit' => 'Módosítás',
+    'settings.view' => 'Megtekintés',
+
+    'options.none' => '- nincs beállítva -',
+    'options.google' => 'Google',
+    'options.ddg' => 'DuckDuckGo',
+    'options.bing' => 'Bing',
+    'options.qwant' => 'Qwant',
+    'options.startpage' => 'Kezdő oldal',
+    'options.yes' => 'Igen',
+    'options.no' => 'Nem',
+    'options.nzbhydra' => 'NZBHydra',
+    'options.jackett' => 'Jackett',
+
+    'buttons.save' => 'Mentás',
+    'buttons.cancel' => 'Mégsem',
+    'buttons.add' => 'Hozzáadás',
+    'buttons.upload' => 'Ikon feltöltése',
+    'buttons.downloadapps' => 'Alkalmazás lista frissítése',
+
+    'dash.pin_item' => 'Kitőzés kezdő képernyőre',
+    'dash.no_apps' => 'Jelenleg nincsenek kitűzött alkalmazások, :link1 or :link2',
+    'dash.link1' => 'Alkalmazás hozzáadása',
+    'dash.link2' => 'Elem kitűzése kezdőképernyőre',
+    'dash.pinned_items' => 'Kitűzött elemek',
+
+    'apps.app_list' => 'Alkalmazás lista',
+    'apps.view_trash' => 'Törölt elemek megtekintése',
+    'apps.add_application' => 'Alkalmazás hozzáadása',
+    'apps.application_name' => 'Alkalmazás neve',
+    'apps.colour' => 'Szín',
+    'apps.icon' => 'Ikon',
+    'apps.pinned' => 'Kitűzve',
+    'apps.title' => 'Cím',
+    'apps.hex' => 'Hexa színkód',
+    'apps.username' => 'Felhasználói név',
+    'apps.password' => 'Jelszó',
+    'apps.config' => 'Konfiguráció',
+    'apps.apikey' => 'API Kulcs',
+    'apps.enable' => 'Engedélyezés',
+    'apps.tag_list' => 'Címke lista',
+    'apps.add_tag' => 'Címke hozzáadása',
+    'apps.tag_name' => 'Címke név',
+    'apps.tags' => 'Címke',
+    'apps.override' => 'Eltérő URL alkaplazása.',
+    'apps.preview' => 'Előnézet',
+    'apps.apptype' => 'Alkalmazás Típus',
+
+    'dashboard' => 'Kezdőképernyő',
+
+    'user.user_list' => 'Felhasználük',
+    'user.add_user' => 'Felhasználó hozzáadása',
+    'user.username' => 'Felhasználói név',
+    'user.avatar' => 'Avatar',
+    'user.email' => 'Email',
+    'user.password_confirm' => 'Jelszó megerősítés',
+    'user.secure_front' => 'Nyilvános hozzáférés engedélyezése - Csak jelszó alkalmazása esetén.',
+    'user.autologin' => 'Bejelentkezés URL-el, Link birtokában bárki bejelentkezhet.',
+	
+	
+    'url' => 'URL',
+    'title' => 'Cím',
+    'delete' => 'Törlés',
+    'optional' => 'Opcionális',
+    'restore' => 'Visszaállítás',
+
+    'alert.success.item_created' => 'Elem sikeresen létrehozva',
+    'alert.success.item_updated' => 'Elem sikeresen frissítve',
+    'alert.success.item_deleted' => 'Elem sikeresen törölve',
+    'alert.success.item_restored' => 'Elem sikeresen visszaállítva',
+    'alert.success.updating' => 'Alkalmazás lista frissítése',
+
+    'alert.success.tag_created' => 'Címkre sikeresen létrehozva',
+    'alert.success.tag_updated' => 'Címke sikeresen frissítve',
+    'alert.success.tag_deleted' => 'Címke sikeresen törölve',
+    'alert.success.tag_restored' => 'Címke sikeresen visszaállítva',
+
+    'alert.success.setting_updated' => 'Sikeres beállítás módosítás.',
+    'alert.error.not_exist' => 'Nem létező beállítás',
+
+    'alert.success.user_created' => 'Felhasználó sikeresen létrehozva',
+    'alert.success.user_updated' => 'Felhasználó sikeresen frissítve',
+    'alert.success.user_deleted' => 'Felhasználó sikeresen törölve',
+    'alert.success.user_restored' => 'Felhasználó sikeresen visszaállítva',
+
+
+];
