fd3494684dfcbeeaa3b064d54e6dd39d3d405f5c
Vincent <vc5@users.noreply.github.com>
2021-06-20 15:29:17 +0800

    translation for chinese

---
+++ resources/lang/cn/app.php
@@ -0,0 +1,120 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | App Language Lines
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'settings.system' => '系统',
+    'settings.appearance' => '显示',
+    'settings.miscellaneous' => '杂项',
+    'settings.support' => '支持',
+    'settings.donate' => '捐赠',
+
+    'settings.version' => '版本号',
+    'settings.background_image' => '背景图片',
+    'settings.window_target' => '链接打开方式',
+    'settings.window_target.current' => '当前标签页打开',
+    'settings.window_target.one' => '同一窗口打开',
+    'settings.window_target.new' => '在新标签页打开',
+    'settings.homepage_search' => '主页搜索栏',
+    'settings.search_provider' => '默认搜索提供方',
+    'settings.language' => '语言',
+    'settings.reset' => '重置回默认设置',
+    'settings.remove' => '移除',
+    'settings.search' => '搜索',
+    'settings.no_items' => '找不到对象',
+
+
+    'settings.label' => '选项',
+    'settings.value' => '参数',
+    'settings.edit' => '编辑',
+    'settings.view' => '查看',
+
+    'options.none' => '- 未设定 -',
+    'options.google' => 'Google',
+    'options.ddg' => 'DuckDuckGo',
+    'options.bing' => 'Bing',
+    'options.qwant' => 'Qwant',
+    'options.startpage' => 'StartPage',
+    'options.yes' => 'Yes',
+    'options.no' => 'No',
+    'options.nzbhydra' => 'NZBHydra',
+    'options.jackett' => 'Jackett',
+
+    'buttons.save' => '保存',
+    'buttons.cancel' => '取消',
+    'buttons.add' => '添加',
+    'buttons.upload' => '上传图标',
+    'buttons.downloadapps' => '更新应用列表',
+
+    'dash.pin_item' => '将应用钉在展示墙',
+    'dash.no_apps' => '当前没有卡片钉在展示墙, :link1 或者 :link2',
+    'dash.link1' => '在当前位置添加一个应用',
+    'dash.link2' => '钉一个卡片到当前位置',
+    'dash.pinned_items' => '已钉住卡片',
+
+    'apps.app_list' => '应用列表',
+    'apps.view_trash' => '查看回收站',
+    'apps.add_application' => '添加应用',
+    'apps.application_name' => '应用名称',
+    'apps.colour' => '颜色',
+    'apps.icon' => '图标',
+    'apps.pinned' => '钉住',
+    'apps.title' => '标题',
+    'apps.hex' => '十六进制颜色',
+    'apps.username' => '用户名',
+    'apps.password' => '密码',
+    'apps.config' => '配置',
+    'apps.apikey' => '配置密钥',
+    'apps.enable' => '允许',
+    'apps.tag_list' => '标签列表',
+    'apps.add_tag' => '添加标签',
+    'apps.tag_name' => '标签名称',
+    'apps.tags' => '标签',
+    'apps.override' => '若链接不同',
+    'apps.preview' => '预览',
+    'apps.apptype' => '应用类型',
+
+    'dashboard' => '主页展示墙',
+
+    'user.user_list' => '用户',
+    'user.add_user' => '添加用户',
+    'user.username' => '用户名',
+    'user.avatar' => '昵称',
+    'user.email' => '邮箱',
+    'user.password_confirm' => '确认密码',
+    'user.secure_front' => '允许通过公共链接登录 - 仅当密码已经设置的情况下.',
+    'user.autologin' => '允许一个特定链接登录. 任何人都可以通过该链接登录.',
+
+    'url' => '链接',
+    'title' => '标题',
+    'delete' => '删除',
+    'optional' => '选项',
+    'restore' => '还原',
+
+    'alert.success.item_created' => '卡片创建成功',
+    'alert.success.item_updated' => '卡片更新成功',
+    'alert.success.item_deleted' => '卡片删除成功',
+    'alert.success.item_restored' => '卡片还原成功',
+    'alert.success.updating' => '更新卡片列表成功',
+
+    'alert.success.tag_created' => '标签创建成功',
+    'alert.success.tag_updated' => '标签更新成功',
+    'alert.success.tag_deleted' => '标签删除成功',
+    'alert.success.tag_restored' => '标签还原成功',
+
+    'alert.success.setting_updated' => '恭喜你，修改设置成功',
+    'alert.error.not_exist' => '该项设置不存在.',
+
+    'alert.success.user_created' => '用户创建成功',
+    'alert.success.user_updated' => '用户更新成功',
+    'alert.success.user_deleted' => '用户删除成功',
+    'alert.success.user_restored' => '用户还原成功',
+
+
+];
