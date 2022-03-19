0f498eca75b86e371b712c7cfea3040e3b0adbdd
Shift <shift@laravelshift.com>
2022-03-19 13:54:34 +0000

    Remove explicit `deleted_at` date cast

--- app/Item.php
+++ app/Item.php
@@ -33,12 +33,7 @@ class Item extends Model
         'title', 'url', 'colour', 'icon', 'appdescription', 'description', 'pinned', 'order', 'type', 'class', 'user_id', 'appid',
     ];
 
-    /**
-     * The attributes that should be mutated to dates.
-     *
-     * @var array
-     */
-    protected $dates = ['deleted_at'];
+
 
     /**
      * Scope a query to only include pinned items.
