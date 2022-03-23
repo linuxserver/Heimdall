80aa12008619ef23a8c42d199cbd6184f9fb9c1e
s4kura0ne <xiazeyu_2011@126.com>
2022-03-23 09:28:39 +0800

    solved Russian readme conflict

---
+++ resources/lang/zh/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => '没有找到该记录',
+    'throttle' => '登录尝试次数太多, 请在 :seconds 后再试',
+
+];
