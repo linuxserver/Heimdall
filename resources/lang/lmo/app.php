62d9b0cc760ba26d7d4a1c30f2c2106ad9b8fea1
Brian Sciretti <45018431+bsciretti@users.noreply.github.com>
2022-05-28 01:56:47 +0200

    Create app.php

---
+++ resources/lang/lmo/app.php
@@ -0,0 +1,218 @@
+<?php
+
+return [
+
+    /*
+
+    |--------------------------------------------------------------------------
+
+    | App Language Lines
+
+    |--------------------------------------------------------------------------
+
+    |
+
+    */
+
+    'settings.system' => 'System',
+
+    'settings.appearance' => 'Appearance',
+
+    'settings.miscellaneous' => 'Miscellaneous',
+
+    'settings.advanced' => 'Advanced',
+
+    'settings.support' => 'Support',
+
+    'settings.donate' => 'Donate',
+
+    'settings.version' => 'Version',
+
+    'settings.background_image' => 'Background Image',
+
+    'settings.window_target' => 'Link opens in',
+
+    'settings.window_target.current' => 'Open in this tab',
+
+    'settings.window_target.one' => 'Open in the same tab',
+
+    'settings.window_target.new' => 'Open in a new tab',
+
+    'settings.homepage_search' => 'Homepage Search',
+
+    'settings.search_provider' => 'Default Search Provider',
+
+    'settings.language' => 'Language',
+
+    'settings.reset' => 'Reset back to default',
+
+    'settings.remove' => 'Remove',
+
+    'settings.search' => 'search',
+
+    'settings.no_items' => 'No items found',
+
+    'settings.label' => 'Label',
+
+    'settings.value' => 'Value',
+
+    'settings.edit' => 'Edit',
+
+    'settings.view' => 'View',
+
+    'settings.custom_css' => 'Custom CSS',
+
+    'settings.custom_js' => 'Custom JavaScript',
+
+    'options.none' => '- not set -',
+
+    'options.google' => 'Google',
+
+    'options.ddg' => 'DuckDuckGo',
+
+    'options.bing' => 'Bing',
+
+    'options.qwant' => 'Qwant',
+
+    'options.startpage' => 'StartPage',
+
+    'options.yes' => 'Yes',
+
+    'options.no' => 'No',
+
+    'options.nzbhydra' => 'NZBHydra',
+
+    'options.jackett' => 'Jackett',
+
+    'buttons.save' => 'Save',
+
+    'buttons.cancel' => 'Cancel',
+
+    'buttons.add' => 'Add',
+
+    'buttons.upload' => 'Upload an icon',
+
+    'buttons.downloadapps' => 'Update Apps List',
+
+    'dash.pin_item' => 'Pin item to dashboard',
+
+    'dash.no_apps' => 'There are currently no pinned applications, :link1 or :link2',
+
+    'dash.link1' => 'Add an application here',
+
+    'dash.link2' => 'Pin an item to the dash',
+
+    'dash.pinned_items' => 'Pinned Items',
+
+    'apps.app_list' => 'Application list',
+
+    'apps.view_trash' => 'View trash',
+
+    'apps.add_application' => 'Add application',
+
+    'apps.application_name' => 'Application name',
+
+    'apps.colour' => 'Colour',
+
+    'apps.icon' => 'Icon',
+
+    'apps.pinned' => 'Pinned',
+
+    'apps.title' => 'Title',
+
+    'apps.hex' => 'Hex colour',
+
+    'apps.username' => 'Username',
+
+    'apps.password' => 'Password',
+
+    'apps.config' => 'Config',
+
+    'apps.apikey' => 'API Key',
+
+    'apps.enable' => 'Enable',
+
+    'apps.tag_list' => 'Tags list',
+
+    'apps.add_tag' => 'Add tag',
+
+    'apps.tag_name' => 'Tag name',
+
+    'apps.tags' => 'Tags',
+
+    'apps.override' => 'If different to main url',
+
+    'apps.preview' => 'Preview',
+
+    'apps.apptype' => 'Application Type',
+
+    'apps.website' => 'Website',
+
+    'apps.description' => 'Description',
+
+    'apps.only_admin_account' => 'Only if you have admin-account!',
+
+    'apps.autologin_url' => 'Auto login url',
+
+    'apps.show_deleted' => 'Showing Deleted Applications',
+
+    'dashboard' => 'Home dashboard',
+
+    'user.user_list' => 'Users',
+
+    'user.add_user' => 'Add user',
+
+    'user.username' => 'Username',
+
+    'user.avatar' => 'Avatar',
+
+    'user.email' => 'Email',
+
+    'user.password_confirm' => 'Confirm Password',
+
+    'user.secure_front' => 'Allow public access to front - Only enforced if a password is set.',
+
+    'user.autologin' => 'Allow logging in from a specific URL. Anyone with the link can login.',
+
+    'url' => 'URL',
+
+    'title' => 'Title',
+
+    'delete' => 'Delete',
+
+    'optional' => 'Optional',
+
+    'restore' => 'Restore',
+
+    'alert.success.item_created' => 'Item created successfully',
+
+    'alert.success.item_updated' => 'Item updated successfully',
+
+    'alert.success.item_deleted' => 'Item deleted successfully',
+
+    'alert.success.item_restored' => 'Item restored successfully',
+
+    'alert.success.updating' => 'Updating apps list',
+
+    'alert.success.tag_created' => 'Tag created successfully',
+
+    'alert.success.tag_updated' => 'Tag updated successfully',
+
+    'alert.success.tag_deleted' => 'Tag deleted successfully',
+
+    'alert.success.tag_restored' => 'Tag restored successfully',
+
+    'alert.success.setting_updated' => 'You have successfully edited this setting',
+
+    'alert.error.not_exist' => 'This setting does not exist.',
+
+    'alert.success.user_created' => 'User created successfully',
+
+    'alert.success.user_updated' => 'User updated successfully',
+
+    'alert.success.user_deleted' => 'User deleted successfully',
+
+    'alert.success.user_restored' => 'User restored successfully',
+
+];
+
