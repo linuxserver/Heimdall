fc023401f509823372126569facf2c300d3306df
Kode <kodestar@gmail.com>
2022-06-29 13:13:00 +0100

    remove heimdall specific xsrf token as clearly not working

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -20,38 +20,4 @@ class VerifyCsrfToken extends Middleware
         //'get_stats'
     ];
 
-    /**
-     * Add the CSRF token to the response cookies.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Symfony\Component\HttpFoundation\Response  $response
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    protected function addCookieToResponse($request, $response)
-    {
-        $config = config('session');
-
-        if ($response instanceof Responsable) {
-            $response = $response->toResponse($request);
-        }
-
-        $response->headers->setCookie(
-            new Cookie(
-                'HEIMDALL-XSRF-TOKEN', $request->session()->token(), $this->availableAt(60 * $config['lifetime']),
-                $config['path'], $config['domain'], $config['secure'], false, false, $config['same_site'] ?? null
-            )
-        );
-
-        return $response;
-    }
-
-    /**
-     * Determine if the cookie contents should be serialized.
-     *
-     * @return bool
-     */
-    public static function serialized()
-    {
-        return EncryptCookies::serialized('HEIMDALL-XSRF-TOKEN');
-    }
 }
