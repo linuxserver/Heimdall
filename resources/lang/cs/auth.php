6565dfde07492aed0c6bd79314f2743e40160b85
Martin Smelik <smelikmartin@gmail.com>
2022-11-17 10:27:43 +0100

    Added czech language

---
+++ resources/lang/cs/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Zadané údaje se neshodují s našimi záznamy.',
+    'throttle' => 'Příliš mnoho pokusů o přihlášení. Prosím opakujte za :seconds sekund.',
+
+];
