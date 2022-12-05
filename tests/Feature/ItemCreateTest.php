6b93f8ed5cd28b06bbeedb34379d758971891ae2
Attila Kerekes <439392+keriati@users.noreply.github.com>
2022-12-05 15:28:18 +0000

    test: Add item create test (#1058)

--- tests/Feature/ItemCreateTest.php
+++ tests/Feature/ItemCreateTest.php
@@ -18,9 +18,6 @@ class ItemCreateTest extends TestCase
         $response->assertStatus(200);
     }
 
-    /**
-     * @return void
-     */
     public function test_display_the_home_dashboard_tag()
     {
         $this->seed();
@@ -28,5 +25,43 @@ class ItemCreateTest extends TestCase
         $response = $this->get('/items/create');
 
         $response->assertSee('Home dashboard');
+    }
+
+    public function test_creates_a_new_item()
+    {
+        $this->seed();
+        $item = [
+            'pinned' => 1,
+            'appid' => 'null',
+            'website' => null,
+            'title' => 'Item A',
+            'colour' => '#00f',
+            'url' => 'http://10.0.1.1',
+            'tags' => [0],
+        ];
+
+        $response = $this->post('/items', $item);
+
+        $response->assertStatus(302);
+        $response->assertSee('Redirecting to');
+    }
+
+    public function test_redirects_to_dash_when_adding_a_new_item()
+    {
+        $this->seed();
+        $item = [
+            'pinned' => 1,
+            'appid' => 'null',
+            'website' => null,
+            'title' => 'Item A',
+            'colour' => '#00f',
+            'url' => 'http://10.0.1.1',
+            'tags' => [0],
+        ];
+
+        $response = $this->post('/items', $item);
+
+        $response->assertStatus(302);
+        $response->assertSee('Redirecting to http://localhost');
     }
 }
