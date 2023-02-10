1ad5603c3ebe0caff9ff891b53ac6f2c214b3b1a
angrystar170 <angrystar170@gmail.com>
2023-02-11 05:30:38 +0900

    Update Korean language. (#1116)

--- resources/lang/ko/auth.php
+++ resources/lang/ko/auth.php
@@ -1,6 +1,6 @@
 <?php
 
 return array (
-  'failed' => 'These credentials do not match our records.',
-  'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+  'failed' => '이 자격 증명은 우리 기록과 일치하지 않습니다.',
+  'throttle' => '로그인 시도 횟수가 너무 많습니다. :seconds 초 후에 다시 시도하십시오.',
 );
