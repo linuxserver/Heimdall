71be27039988ddf0afe04c0473279eec92d33fd5
Anyblade <97947921+Anyblade@users.noreply.github.com>
2023-02-22 15:33:52 +0100

    Add overleaf app

---
+++ Overleaf/config.blade.php
@@ -0,0 +1,19 @@
+<h2>{{ __('app.apps.config') }} ({{ __('app.optional') }}) @include('items.enable')</h2>
+<div class="items">
+    <div class="input">
+        <label>{{ strtoupper(__('app.url')) }}</label>
+        {!! Form::text('config[override_url]', null, array('placeholder' => __('app.apps.override'), 'id' => 'override_url', 'class' => 'form-control')) !!}
+    </div>
+    <div class="input">
+        <label>{{ __('app.apps.username') }}</label>
+        {!! Form::text('config[username]', null, array('placeholder' => __('app.apps.username'), 'data-config' => 'username', 'class' => 'form-control config-item')) !!}
+    </div>
+    <div class="input">
+        <label>{{ __('app.apps.password') }}</label>
+        {!! Form::input('password', 'config[password]', '', ['placeholder' => __('app.apps.password'), 'data-config' => 'password', 'class' => 'form-control config-item']) !!}
+    </div>
+    <div class="input">
+        <button style="margin-top: 32px;" class="btn test" id="test_config">Test</button>
+    </div>
+</div>
+
