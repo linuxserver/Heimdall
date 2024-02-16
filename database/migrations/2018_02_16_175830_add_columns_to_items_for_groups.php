1c73cf08de33a4d52e1372a85b2f225959d986eb
Shift <shift@laravelshift.com>
2024-02-16 20:33:46 +0000

    Adopt anonymous migrations

--- database/migrations/2018_02_16_175830_add_columns_to_items_for_groups.php
+++ database/migrations/2018_02_16_175830_add_columns_to_items_for_groups.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddColumnsToItemsForGroups extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -29,4 +29,4 @@ class AddColumnsToItemsForGroups extends Migration
             $table->dropColumn(['type']);
         });
     }
-}
+};
--- database/migrations/2022_03_16_093044_add_class_to_application.php
+++ database/migrations/2022_03_16_093044_add_class_to_application.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddClassToApplication extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -29,4 +29,4 @@ class AddClassToApplication extends Migration
             $table->dropColumn(['class']);
         });
     }
-}
+};
