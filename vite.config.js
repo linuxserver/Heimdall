3a3cea437bc2b9441b60fe0026e4b6406f04a03e
Shift <shift@laravelshift.com>
2024-02-17 19:03:23 +0000

    Configure Vite

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/assets/sass/app.scss',
+            ],
+            refresh: true,
+        }),
+    ],
+});
