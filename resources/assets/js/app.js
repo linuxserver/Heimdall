b7c0fd29c6e42e6b7764daa31046e724332d0e3f
Chris Hunt <chris.hunt@kasmweb.com>
2024-02-18 18:33:47 +0000

    Fix extra prettier complaints

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -173,12 +173,12 @@ $.when($.ready).then(() => {
     })
     .on("click", ".tag", (e) => {
       e.preventDefault();
-      const tag =  $(e.target).data("tag")
-      $("#taglist .tag").removeClass("current")
-      $(e.target).addClass("current")
-      $("#sortable .item-container").show()
+      const tag = $(e.target).data("tag");
+      $("#taglist .tag").removeClass("current");
+      $(e.target).addClass("current");
+      $("#sortable .item-container").show();
       if (tag !== "all") {
-        $("#sortable .item-container:not(."+tag+")").hide()
+        $("#sortable .item-container:not(." + tag + ")").hide();
       }
     })
     .on("click", "#add-item, #pin-item", (e) => {
