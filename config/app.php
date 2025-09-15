54cf2b88ca3164c1123c21c97850835f1bc039ae
KodeStar <kodestar@gmail.com>
2025-09-15 16:28:00 +0100

    Update application version to 2.7.6

--- config/app.php
+++ config/app.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Facade;
 
 return [
 
-    'version' => '2.7.5',
+    'version' => '2.7.6',
 
     'appsource' => env('APP_SOURCE', 'https://appslist.heimdall.site/'),
 
